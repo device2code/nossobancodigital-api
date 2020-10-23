75f201536c6a42081f94b468ff648c31e0f1c6a3
Andre Scherrer <andrescherrer@gmail.com>
2020-10-22 22:57:58 -0300

    setup do projeto

---
+++ src/main/java/br/com/device2code/nossobancodigitalapi/NossobancodigitalApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.device2code.nossobancodigitalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossobancodigitalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossobancodigitalApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/device2code/nossobancodigitalapi/NossobancodigitalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.device2code.nossobancodigitalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossobancodigitalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
